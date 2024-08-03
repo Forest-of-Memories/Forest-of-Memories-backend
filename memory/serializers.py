ce7ff028ff15b59aac543be145f659f6c8932240
Seohyun <tjgus9138@naver.com>
2024-08-04 03:50:51 +0900

    질문 리스트 GET 완성

--- memory/serializers.py
+++ memory/serializers.py
@@ -7,13 +7,13 @@ from .models import CommonComment, PersonalComment
 # 공통 질문
 class CommonQuestionSerializer(serializers.ModelSerializer):
     class Meta:
-      model = CommonQuestion
-      fields = '__all__'
+        model = CommonQuestion
+        fields = ['cmn_qst_no', 'cmn_qst_txt']
 
 class PersonalQuestionSerializer(serializers.ModelSerializer):
     class Meta:
-      model = PersonalQuestion
-      fields = ['prsn_qst_no', 'prsn_qst_txt']
+        model = PersonalQuestion
+        fields = ['prsn_qst_no', 'prsn_qst_txt']
 
 class FeedSerializer(serializers.ModelSerializer):
     class Meta:
--- memory/urls.py
+++ memory/urls.py
@@ -1,19 +1,11 @@
-from django.urls import path
-from .views import CommonQuestionViewSet, PersonalQuestionViewSet, FeedViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CommonQuestionViewSet, PersonalQuestionViewSet
 
-common_question_list = CommonQuestionViewSet.as_view({
-    'get': 'list'
-})
-
-personal_question_list = PersonalQuestionViewSet.as_view({
-    'get': 'list'
-})
-
-
+router = DefaultRouter()
+router.register(r'common-questions', CommonQuestionViewSet, basename='common-questions')
+router.register(r'personal-questions', PersonalQuestionViewSet, basename='personal-questions')
 
 urlpatterns = [
-  path('common-questions-list/<int:user_id>/', common_question_list),
-  path('personal-questions-list/<int:user_id>/', personal_question_list),
-  path('feed/', FeedViewSet.as_view({'get': 'list', 'post': 'create'})),
-  path('feed/<int:feed_id>/', FeedViewSet.as_view({'get': 'retrieve', 'put':'update', 'patch': 'partial_update', 'delete': 'destroy'})),
+    path('', include(router.urls)),
 ]
