8a672a9a2b1c3a7ae1fcb95331f8ffe57a17661e
Novrule <2000dnjswns@naver.com>
2024-08-04 16:04:04 +0900

    배포 준비

--- config/settings.py
+++ config/settings.py
@@ -170,4 +170,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:8000',
+    'https://port-0-forest-of-memories-backend-pn2llx4kscst.sel5.cloudtype.app/',
 ]
